407215e98c2ca2973c5d291a917dfe1d96574b37
andrei-mak <andrei.v.mak@gmail.com>
2013-08-27 17:16:29 +0400

    Create ExpMySms
    Fragments version

---
+++ ExpMySms/src/com/example/expmysms/ExpActivity.java
@@ -0,0 +1,84 @@
+package com.example.expmysms;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.Menu;
+import android.view.View;
+
+public class ExpActivity extends FragmentActivity {
+
+	private static final int NUM_PAGES = 2;
+
+	Fragment f = null;
+
+	/**
+	 * The pager widget, which handles animation and allows swiping horizontally
+	 * to access previous and next wizard steps.
+	 */
+	private ViewPager mPager;
+
+	/**
+	 * The pager adapter, which provides the pages to the view pager widget.
+	 */
+	private PagerAdapter mPagerAdapter;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_exp);
+
+		// Instantiate a ViewPager and a PagerAdapter.
+		mPager = (ViewPager) findViewById(R.id.pager);
+		mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+		mPager.setAdapter(mPagerAdapter);
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.exp, menu);
+		return true;
+	}
+
+	public void onClickExportMySms(View view) {
+		mPager.setCurrentItem(mPager.getCurrentItem() + 1, true);
+	}
+
+	/**
+	 * A simple pager adapter that represents 5 ScreenSlidePageFragment objects,
+	 * in sequence.
+	 */
+	private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+		public ScreenSlidePagerAdapter(FragmentManager fm) {
+			super(fm);
+		}
+
+		@Override
+		public Fragment getItem(int position) {
+			// return new ExpFragment1(); // in case all same
+
+			switch (position) {
+			case 0:
+				f = new ExpFragment1();
+				break;
+			case 1:
+				f = new ExpFragment2();
+				break;
+			}
+			return f;
+
+		}
+
+		@Override
+		public int getCount() {
+			return NUM_PAGES;
+		}
+	}
+
+}
---
+++ ExpMySms/src/com/example/expmysms/ExpFragment2.java
@@ -0,0 +1,20 @@
+package com.example.expmysms;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ExpFragment2 extends Fragment {
+
+		@Override
+	    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	            Bundle savedInstanceState) {
+	        ViewGroup rootView = (ViewGroup) inflater.inflate(
+	                R.layout.fragment_exp2, container, false);
+
+	        return rootView;
+
+	}
+}
